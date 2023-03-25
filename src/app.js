8c7bcd2479e554e8d8da0ff9f6283bfdc4db0b79
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2023-03-25 20:20:52 -0300

    :white_check_mark: Requisito 04
    Req 04

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const controller = require('./controllers/productsController');
 const app = express();
 app.use(json());
 const errorHandler = require('./middleware/errorHandler');
+const { validationProductName } = require('./middleware/productsMiddleware');
 
 app.get('/', (_request, response) => {
   response.send();
@@ -14,7 +15,7 @@ app.get('/products/', controller.findAllProducts);
 
 app.get('/products/:id', controller.findProductById);
 
-app.post('/products', controller.createProducts);
+app.post('/products', validationProductName, controller.createProducts);
 
 app.use(errorHandler);
 module.exports = app;
--- src/middleware/productsMiddleware.js
+++ src/middleware/productsMiddleware.js
@@ -1,1 +1,14 @@
-// const validationProduct = (name) => { }
+const validationProductName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  if (name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be a least 5 characters long' });
+  }
+  next();
+};
+
+module.exports = {
+  validationProductName,
+};
